fca634fe409c7a8d9aed8798283b4b923640d25e
katukurijaswanth2 <katukurijaswanth2@gmail.com>
2025-04-27 15:25:07 +0530

    second  commit

---
+++ bankaccount.java
@@ -0,0 +1,11 @@
+class bankaccount {
+    static int accountno = 142214208;
+    static String accountname = "jaswanth";
+    static int accountbal = 5000;
+
+    public static void main(String[] args) {
+        System.out.println("account number " + accountno);
+        System.out.println("account name " + accountname);
+        System.out.println("accountbalance " + accountbal);
+    }
+}
